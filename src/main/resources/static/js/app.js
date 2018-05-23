8d0bf03818440cb852e0d20d088fb365427f94a1
robertokbs <robertokbs@gmail.com>
2018-05-23 17:02:02 -0300

    salvando cliente sem atualizar tela

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -8,19 +8,36 @@ appCliente.controller("indexController", function($scope, $http){
 	$scope.nome = "roberto";
 	$scope.sobrenome = "sousa";
 	$scope.clientes = [];
+	$scope.cliente = {};
+	
+	$scope.carregarClientes = function(){
+		$http({method:'GET', url:'http://localhost:8080/clientes'})
+		.then(function(response){
+			$scope.clientes = response.data;
+			
+			console.log(response.data);
+			console.log(response.status);
+		} , function(response){
+			console.log(response.data);
+			console.log(response.status);
+		} );
+	
+	};
 	
 	
-	$http({method:'GET', url:'http://localhost:8080/clientes'})
-	.then(function(response){
-		
-		$scope.clientes = response.data;
-		
-		console.log(response.data);
-		console.log(response.status);
-	} , function(response){
-		console.log(response.data);
-		console.log(response.status);
-	} );
+	$scope.salvarCliente= function(){
+		$http({method:'POST', url:'http://localhost:8080/clientes', data:$scope.cliente})
+		.then(function(response){
+			$scope.clientes = response.data;
+			
+			console.log(response.data);
+			console.log(response.status);
+		} , function(response){
+			console.log(response.data);
+			console.log(response.status);
+		} );
+	};
 	
 	
+	$scope.carregarClientes();
 });
