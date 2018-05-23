aeb643b19281babbcf10fa950abd0e6174f17276
robertokbs <robertokbs@gmail.com>
2018-05-23 15:55:00 -0300

    aula 04 parte 1, requisicao do json , impressao no console

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,9 +3,19 @@ var appCliente = angular.module("appCliente", []);
 
 
 //criacao de controllers
-appCliente.controller("indexController", function($scope){
+appCliente.controller("indexController", function($scope, $http){
 	
 	$scope.nome = "roberto";
 	$scope.sobrenome = "sousa";
+	
+	$http({method:'GET', url:'http://localhost:8080/clientes'})
+	.then(function(response){
+		console.log(response.data);
+		console.log(response.status);
+	} , function(response){
+		console.log(response.data);
+		console.log(response.status);
+	} );
+	
 	
 });
